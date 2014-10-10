9d609edcd63f42f45bde7918962019cf211f7f22
Jake Barlow <jake@jakebarlow.com>
2014-10-10 15:07:27 -0600

    =Config settting bug

--- app/config/usaepay.php
+++ app/config/usaepay.php
@@ -2,7 +2,7 @@
 return [
 	'sourcekey' => 'O0L4JhjLZOe8Ct067sm5Yp6OlKzqBkIM',
 	'pin' => '9ehAJE7',
-	'test_mode'=>false	
+	'test_mode'=>false,	
 	//development credentials
 	'sourcekey' => '_ppKcs9upz9uctAemahd27aOQCm4GOM3',
 	'pin' => '123456',
