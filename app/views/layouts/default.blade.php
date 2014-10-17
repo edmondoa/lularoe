1e2e79418dfbcb2a646f06c6148d21ddaf27a1ca
Stephen Gashler <sgashler@controlpad.com>
2014-10-17 10:17:29 -0600

    no message

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -71,25 +71,25 @@
  		@if (Auth::check() && (Auth::user()->hasRole(['Admin','Superadmin'])))
  		<div class="col-xs-6 col-sm-3 col-md-2 sidebar-offcanvas" id="sidebar" role="navigation">
 			<div class="list-group">
-				<a href="{{ url('addresses') }}" class="list-group-item">Addresses</a>
-				<a href="{{ url('bonuses') }}" class="list-group-item">Bonuses</a>
-				<a href="{{ url('cart') }}" class="list-group-item">Cart</a>
+				<a href="{{ url('addresses') }}" class="list-group-item"><i class="fa fa-home"></i> Addresses</a>
+				<a href="{{ url('bonuses') }}" class="list-group-item"><i class="fa fa-certificate"></i> Bonuses</a>
+				<a href="{{ url('cart') }}" class="list-group-item"><i class="fa fa-shopping-cart"></i> Cart</a>
 				<!-- <a href="{{ url('commissions') }}" class="list-group-item">Commissions</a> -->
-				<a href="{{ url('emailMessages') }}" class="list-group-item">EmailMessages</a>
-				<a href="{{ url('images') }}" class="list-group-item">Images</a>
-				<a href="{{ url('levels') }}" class="list-group-item">Levels</a>
-				<a href="{{ url('pages') }}" class="list-group-item">Pages</a>
+				<a href="{{ url('emailMessages') }}" class="list-group-item"><i class="fa fa-envelope"></i> EmailMessages</a>
+				<a href="{{ url('images') }}" class="list-group-item"><i class="fa fa-file-image-o"></i> Images</a>
+				<a href="{{ url('levels') }}" class="list-group-item"><i class="fa fa-sitemap"></i> Levels</a>
+				<a href="{{ url('pages') }}" class="list-group-item"><i class="fa fa-file-o"></i> Pages</a>
 				<!-- <a href="{{ url('payments') }}" class="list-group-item">Payments</a> -->
 				<a href="{{ url('products') }}" class="list-group-item"><i class="fa fa-mobile-phone"></i> &nbsp;Products</a>
-				<a href="{{ url('profiles') }}" class="list-group-item">Profiles</a>
-				<a href="{{ url('ranks') }}" class="list-group-item">Ranks</a>
-				<a href="{{ url('reviews') }}" class="list-group-item">Reviews</a>
-				<a href="{{ url('roles') }}" class="list-group-item">Roles</a>
-				<a href="{{ url('sales') }}" class="list-group-item">Sales</a>
-				<a href="{{ url('users') }}" class="list-group-item"><i class="fa fa-user"></i> Users</a>
-				<a href="{{ url('userProducts') }}" class="list-group-item">UserProducts</a>
-				<a href="{{ url('userRanks') }}" class="list-group-item">UserRanks</a>
-				<a href="{{ url('smsMessages') }}" class="list-group-item">SmsMessages</a>
+				<a href="{{ url('profiles') }}" class="list-group-item"><i class="fa fa-user"></i> Profiles</a>
+				<a href="{{ url('ranks') }}" class="list-group-item"><i class="fa fa-certificate"></i> Ranks</a>
+				<a href="{{ url('reviews') }}" class="list-group-item"><i class="fa fa-comments"></i> Reviews</a>
+				<a href="{{ url('roles') }}" class="list-group-item"><i class="fa fa-user"></i> Roles</a>
+				<a href="{{ url('sales') }}" class="list-group-item"><i class="fa fa-dollar"></i> Sales</a>
+				<a href="{{ url('users') }}" class="list-group-item"><i class="fa fa-users"></i> Users</a>
+				<a href="{{ url('userProducts') }}" class="list-group-item"><i class="fa fa-mobile-phone"></i> UserProducts</a>
+				<a href="{{ url('userRanks') }}" class="list-group-item"><i class="fa fa-certificate"></i> UserRanks</a>
+				<a href="{{ url('smsMessages') }}" class="list-group-item"><i class="fa fa-mobile-phone"></i> SmsMessages</a>
  			</div>
 		</div><!-- col -->
 	 	@endif
