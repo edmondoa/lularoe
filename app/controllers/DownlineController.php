602e029b7d754a521c9e9facc0b079be95481bcc
Stephen Gashler <sgashler@controlpad.com>
2014-10-31 14:56:12 -0600

    Allowed admins to view others downlines

--- app/controllers/DownlineController.php
+++ app/controllers/DownlineController.php
@@ -9,7 +9,7 @@ class DownlineController extends \BaseController
 	
 	public function immediateDownline($id)
 	{
-		if (Auth::user()->hasRepInDownline($id) || Auth::user()->id == $id) {
+		if (Auth::user()->hasRepInDownline($id) || Auth::user()->id == $id || Auth::user()->hasRole(array('Superadmin', 'Admin'))) {
 			$user = User::findOrFail($id);
 			return View::make('downline.immediate', compact('user'));
 		}
@@ -17,7 +17,7 @@ class DownlineController extends \BaseController
 	
 	public function allDownline($id)
 	{
-		if (Auth::user()->hasRepInDownline($id) || Auth::user()->id == $id) {
+		if (Auth::user()->hasRepInDownline($id) || Auth::user()->id == $id || Auth::user()->hasRole(array('Superadmin', 'Admin'))) {
 			$user = User::findOrFail($id);
 			return View::make('downline.all', compact('user'));
 		}
