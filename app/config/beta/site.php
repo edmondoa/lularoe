0d74bafb3be7d0f5076907566745dc84af9453a1
Jake Barlow <jake@jakebarlow.com>
2014-11-14 08:16:48 -0700

    and another

--- app/config/beta/site.php
+++ app/config/beta/site.php
@@ -17,7 +17,7 @@ return [
 	'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
 	'default_from_email' => 'no-reply@sociallymobile.com',
 	'company_name' => 'SociallyMobile - Beta',
-	'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
+	//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
 	'locked_subdomains' => [
 		'my',
 		'beta',
