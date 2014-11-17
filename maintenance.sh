42fe6724693a0e4847e1f888c36a096f45e26ab3
Jake Barlow <jake@jakebarlow.com>
2014-11-17 16:43:15 -0700

    Added a backup script

---
+++ maintenance.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+#@daily /var/www/html/maintenance.sh
+# Parent backup directory
+backup_parent_dir="/var/www/db_backup/"
+
+# MySQL settings
+mysql_user="sm_admin"
+mysql_password="WyHuR$jYGuT&9a8"
+ 
+# Create backup directory and set permissions
+backup_date=`date +%Y_%m_%d_%H_%M_%p`
+backup_dir="${backup_parent_dir}${backup_date}"
+echo "Backup directory: ${backup_dir}"
+mkdir -p "${backup_dir}"
+chmod 755 "${backup_dir}"
+
+# Get MySQL databases
+mysql_databases=`echo 'show databases' | mysql --user=${mysql_user} --password=${mysql_password} -B | sed /^Database$/d`
+ 
+# Backup and compress each database
+for database in $mysql_databases
+do
+  if [ "${database}" == "information_schema" ] || [ "${database}" == "performance_schema" ]; then
+        additional_mysqldump_params="--skip-lock-tables"
+  else
+        additional_mysqldump_params=""
+  fi
+  echo "Creating backup of \"${database}\" database"
+  mysqldump ${additional_mysqldump_params} --user=${mysql_user} --password=${mysql_password} ${database} | gzip > "${backup_dir}/${database}.gz"
+  chmod 775 "${backup_dir}/${database}.gz"
+done
+
+find "${backup_parent_dir}" -maxdepth 1 -type d -mtime +7 -exec rm -f {} \;
