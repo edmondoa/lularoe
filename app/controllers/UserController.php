e32ca0c9f44b7c78f54d771b9dfa4459cd8ea61e
Stephen Gashler <sgashler@controlpad.com>
2015-01-19 08:47:04 -0700

    Fixed user email validation error

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -43,7 +43,7 @@ class userController extends \BaseController {
 		$rules['state'] = 'required';
 		$rules['zip'] = 'required|digits_between:5,10';
 		$rules['dob'] = 'required|before:'.date('Y-m-d',strtotime('18 years ago'));
-		$rules['password'] = 'required|confirmed|digits_between:8,12';
+		$rules['password'] = 'required|confirmed|digits_between:8,25';
 		$rules['sponsor_id'] = 'required|numeric';
 		$check_sponsor_id = User::where('public_id', $data['sponsor_id']);
 
@@ -198,7 +198,7 @@ class userController extends \BaseController {
 			$user = User::findOrFail($id);
 			$old_user_data = $user;
 			$rules = User::$rules;
-			$rules['email'] = 'required|email|unique:users';
+			$rules['email'] = 'required|email|unique:users,email,' . $user->id;
 			$rules['password'] = 'sometimes|confirmed|digits_between:8,25';
 			//$rules['sponsor_id'] = 'required|digits';
 			$data = Input::all();
