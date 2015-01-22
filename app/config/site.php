7cea653f5fd77b6a000fb42b7df1a8578e9cdf8f
Matthew Frederico (cyberteknix) <m.fred@cyberteknix.com>
2015-01-22 16:01:21 -0700

    updates for mail sending
    Conflicts:
    	app/config/site.php

--- app/config/site.php
+++ app/config/site.php
@@ -1,29 +1,66 @@
 <?php
 
-return [
+if (preg_match('/mylularoe.com/',$_SERVER['HTTP_HOST']))
+{
+	return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Site general configutaion
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storage of settings for site
+			/*
+			|--------------------------------------------------------------------------
+			| Site general configutaion
+			|--------------------------------------------------------------------------
+			|
+			| This file is for storage of settings for site
 
-	|'domain' => 'my.sociallymobile.com',
-    'base_domain' => 'sociallymobile.com',
-	*/
-    
+			|
+			*/
 
-	'domain' => 'llr.controlpad.local',
-	'base_domain' => 'llr.controlpad.local',
-	'preregistration_fee' => 100.00,
-	'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
-	'default_from_email' => 'no-reply@lularoe.com',
-	'company_name' => 'LulaRoe',
-	//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
-	'locked_subdomains' => [
-		'my',
-		'beta',
-		'www',
-	]
-];
+			'domain' => 'www.mylularoe.com',
+			'contact_email' => 'mfrederico@gmail.com',
+			'contact_first_name' => 'Matt',
+			'contact_last_name'  => 'Frederico',
+			'base_domain' => 'www.mylularoe.com',
+			'preregistration_fee' => 2000.00,
+			'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
+			'default_from_email' => 'no-reply@mylularoe.com',
+			'company_name' => 'LulaRoe',
+			//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
+			'locked_subdomains' => [
+					'www'
+			],
+			'cache_length' => 10
+	];
+}
+else 
+{
+	return [
+
+		/*
+		|--------------------------------------------------------------------------
+		| Site general configutaion
+		|--------------------------------------------------------------------------
+		|
+		| This file is for storage of settings for site
+
+		|
+		*/
+
+		'domain' => 'my.llr.controlpad.com',
+		'base_domain' => 'llr.controlpad.com',
+		'preregistration_fee' => 100.00,
+		'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
+		'default_from_email' => 'no-reply@mylularoe.com',
+		'contact_email' => 'mfrederico@gmail.com',
+		'contact_first_name' => 'Matt',
+		'contact_last_name'  => 'Frederico',
+		'company_name' => 'LLRDEV',
+		//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
+		'locked_subdomains' => [
+			'my',
+			'beta',
+			'www',
+			'llr'
+		],
+		'cache_length' => 10
+	];
+}
+
