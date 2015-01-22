65bca88d3b03b84010746964b15040b4b53929d4
Matthew Frederico (cyberteknix) <m.fred@cyberteknix.com>
2015-01-22 15:05:06 -0700

    for site moving

--- app/config/site.php
+++ app/config/site.php
@@ -1,29 +1,60 @@
 <?php
 
-return [
+if (preg_match('/mylularoe.com/',$_SERVER['HTTP_HOST']))
+{
+	return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Site general configutaion
-	|--------------------------------------------------------------------------
-	|
-	| This file is for storage of settings for site
+			/*
+			|--------------------------------------------------------------------------
+			| Site general configutaion
+			|--------------------------------------------------------------------------
+			|
+			| This file is for storage of settings for site
 
-	|
-	*/
+			|
+			*/
 
-	'domain' => 'my.llr.controlpad.com',
-	'base_domain' => 'llr.controlpad.com',
-	'preregistration_fee' => 100.00,
-	'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
-	'default_from_email' => 'no-reply@mylularoe.com',
-	'company_name' => 'LLRDEV',
-	//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
-	'locked_subdomains' => [
-		'my',
-		'beta',
-		'www',
-		'llr'
-	],
-	'cache_length' => 10
-];
+			'domain' => 'www.mylularoe.com',
+			'base_domain' => 'www.mylularoe.com',
+			'preregistration_fee' => 2000.00,
+			'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
+			'default_from_email' => 'no-reply@mylularoe.com',
+			'company_name' => 'LulaRoe',
+			//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
+			'locked_subdomains' => [
+					'www'
+			],
+			'cache_length' => 10
+	];
+}
+else 
+{
+	return [
+
+		/*
+		|--------------------------------------------------------------------------
+		| Site general configutaion
+		|--------------------------------------------------------------------------
+		|
+		| This file is for storage of settings for site
+
+		|
+		*/
+
+		'domain' => 'my.llr.controlpad.com',
+		'base_domain' => 'llr.controlpad.com',
+		'preregistration_fee' => 100.00,
+		'new_time_frame' => 86400, // 1 day, /*604800*/ // 1 week
+		'default_from_email' => 'no-reply@mylularoe.com',
+		'company_name' => 'LLRDEV',
+		//'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
+		'locked_subdomains' => [
+			'my',
+			'beta',
+			'www',
+			'llr'
+		],
+		'cache_length' => 10
+	];
+}
+
