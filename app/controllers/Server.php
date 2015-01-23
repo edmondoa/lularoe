6e8a4ace9bea3f22cd805ae1b10bf064eb20f9bb
Stephen Gashler <sgashler@controlpad.com>
2015-01-23 12:26:30 -0700

    Updated deploy-production script

--- app/controllers/Server.php
+++ app/controllers/Server.php
@@ -6,16 +6,19 @@ class Server extends \BaseController {
 	{
 		//return "Beta works!";
 		SSH::into('beta')->run(array(
-			// back up db
-			"php artisan down",
-			"mysqldump -u root --password='Yr*r,dAv$S?qE8,N' soc_mob > /var/www/html/soc_mob_backup_" . date('Y_m_d_h_m') . ".sql",
 			'cd /var/www/html',
-			// 'ls -la',
-			'git pull',
+			'ls -la',
+			//'echo $HOSTNAME',
+			'git checkout develop',
+			'git reset --hard origin/develop',
+			'git pull -f',
 			//'sudo chmod -R 777 app/storage',
-			'composer install --no-dev',
-			'php artisan migrate',
-			"php artisan up"
+			//'mysqldump -u  --password="" --no-data  | grep ^DROP | mysql -u   --password="" database',
+			'composer install',
+			//'php artisan migrate:refresh --seed --env=beta --force'
+			//'php artisan migrate --force',
+			//'php artisan migrate:refresh --force',
+			//'php artisan db:seed --force',
 		), function($line){
 		 
 			echo $line.PHP_EOL; // outputs server feedback
@@ -28,9 +31,9 @@ class Server extends \BaseController {
 		//return "Deploying";
 		SSH::into('production')->run(array(
 			// back up db
-			"php artisan down",
-			"mysqldump -u root --password='Yr*r,dAv$S?qE8,N' soc_mob > /var/www/html/soc_mob_backup_" . date('Y_m_d_h_m') . ".sql",
 			'cd /var/www/html',
+			"php artisan down",
+			"mysqldump -u root --password='Yr*r,dAv" . '$' . "S?qE8,N' soc_mob > /var/www/html/soc_mob_backup_" . date('Y_m_d_h_m') . ".sql",
 			// 'ls -la',
 			'git pull',
 			//'sudo chmod -R 777 app/storage',
