3f139e9a980f7769483158b8b145bf1dcf488623
Randy D. Binondo <binondord@gmail.com>
2015-01-28 12:52:26 +0800

    revert orig settings
    Signed-off-by: Randy D. Binondo <binondord@gmail.com>

--- app/config/database.php
+++ app/config/database.php
@@ -77,76 +77,6 @@ elseif (isset($_SERVER['HTTP_HOST']) && preg_match('/mylularoe.com$/',$_SERVER['
 
 	);
 }
-elseif (isset($_SERVER['HTTP_HOST']) && preg_match('/llr.controlpad.zylun$/',$_SERVER['HTTP_HOST']))
-{
-    return array(
-
-            'fetch' => PDO::FETCH_CLASS,
-            'default' => 'mysql',
-            'connections' => array(
-                    'mysql' => array(
-                            'driver'    => 'mysql',
-                            'host'      => 'localhost',
-                            'database'  => 'llr_dev',
-                            'username'  => 'root',
-                            'password'  => '',
-                            'charset'   => 'utf8',
-                            'collation' => 'utf8_unicode_ci',
-                            'prefix'    => '',
-                    ),
-
-            ),
-
-            'migrations' => 'migrations',
-            'redis' => array(
-
-                    'cluster' => false,
-
-                    'default' => array(
-                            'host'     => '127.0.0.1',
-                            'port'     => 6379,
-                            'database' => 0,
-                    ),
-
-            ),
-
-    );
-}
-elseif (isset($_SERVER['HTTP_HOST']) && preg_match('/intranet.zylun.local/',$_SERVER['HTTP_HOST']))
-{
-    return array(
-
-            'fetch' => PDO::FETCH_CLASS,
-            'default' => 'mysql',
-            'connections' => array(
-                    'mysql' => array(
-                            'driver'    => 'mysql',
-                            'host'      => 'localhost',
-                            'database'  => 'llr_dev',
-                            'username'  => 'root',
-                            'password'  => 'b0X42!!!',
-                            'charset'   => 'utf8',
-                            'collation' => 'utf8_unicode_ci',
-                            'prefix'    => '',
-                    ),
-
-            ),
-
-            'migrations' => 'migrations',
-            'redis' => array(
-
-                    'cluster' => false,
-
-                    'default' => array(
-                            'host'     => '127.0.0.1',
-                            'port'     => 6379,
-                            'database' => 0,
-                    ),
-
-            ),
-
-    );
-}
 // This is the default connection
 else
 {
@@ -159,8 +89,8 @@ else
 				'driver'    => 'mysql',
 				'host'      => 'localhost',
 				'database'  => 'llr_dev',
-				'username'  => 'llr_dev',
-				'password'  => '7U8$SAV*NEjuB$T%',
+				'username'  => 'root',/*'llr_dev',*/
+				'password'  => 'b0X42!!!',/* '7U8$SAV*NEjuB$T%',*/
 				'charset'   => 'utf8',
 				'collation' => 'utf8_unicode_ci',
 				'prefix'    => '',
