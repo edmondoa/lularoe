b17c94e98aa8855436f5f1426f0d34922ea29600
Randy D. Binondo <randyb@zylun.com>
2015-02-03 10:57:27 +0800

    lularoe theme sampler
    Signed-off-by: Randy D. Binondo <randyb@zylun.com>

---
+++ app/views/image/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.default')
+@section('content')
+<div class="create">
+	<div class="row">
+		<div class="col col-md-12">
+			@include('_helpers.breadcrumbs')
+		    <h1 class="no-top">New Image</h1>
+		    {{ Form::open(array('url' => 'images')) }}
+		
+			    
+			    <div class="form-group">
+			        {{ Form::label('type', 'Type') }}
+			        {{ Form::text('type', Input::old('type'), array('class' => 'form-control')) }}
+			    </div>
+			    
+			    <div class="form-group">
+			        {{ Form::label('url', 'Url') }}
+			        {{ Form::text('url', Input::old('url'), array('class' => 'form-control')) }}
+			    </div>
+			    
+			    <div class="form-group">
+			        {{ Form::label('disabled', 'Disabled') }}
+			        {{ Form::text('disabled', Input::old('disabled'), array('class' => 'form-control')) }}
+			    </div>
+			    
+		
+			    {{ Form::submit('Add Image', array('class' => 'btn btn-primary')) }}
+	
+		    {{ Form::close() }}
+	    </div>
+	</div>
+</div>
+@stop
