d7146826641939bb86748f8decab7395a3f404c3
Matthew Frederico <mfrederico@gmail.com>
2015-03-02 01:46:30 -0700

    move the "cart" above

--- app/views/inventory/index.blade.php
+++ app/views/inventory/index.blade.php
@@ -62,7 +62,6 @@
                                         </div>
                                         <div class="col-lg-8 col-md-8 col-sm-9 col-xs-9">
                                             <h4 class="media-heading"> @{{order.model}} - @{{order.size}}</h4>
-                                            <p class="">Some semblance of a description could go here</p>
                                             <div class="row">
                                                 <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
                                                     <div class="input-group">
