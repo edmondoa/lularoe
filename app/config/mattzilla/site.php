bbefa19c53ed7887a0a895eb15a85ec7171ac319
Matthew Frederico <mfrederico@gmail.com>
2015-03-03 21:21:43 -0700

    reduce local site.php

--- app/config/mattzilla/site.php
+++ app/config/mattzilla/site.php
@@ -3,11 +3,6 @@
             'domain' => 'mylularoe.local',
             'contact_email' => 'mfrederico@gmail.com',
             'base_domain' => 'mylularoe.local',
-            'company_name' => 'LulaRoe - Dev',
-            //'subdomain' => Route::getCurrentRoute()->getParameter('subdomain'),
-            'locked_subdomains' => [
-                    'www'
-            ],
-            'cache_length' => 10
+            'company_name' => 'LulaRoe - Dev'
     ];
 
