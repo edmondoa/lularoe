270587d037e45f75a7e76bd421a4cb01a52934f4
Randy D. Binondo <randyb@zylun.com>
2015-03-12 11:36:24 +0800

    set initial value to sponsor id

--- public/js/controllers/userEditController.js
+++ public/js/controllers/userEditController.js
@@ -24,26 +24,35 @@ try {
         /**
         * operations here
         */
-        $scope.sponsor_name = "";
-        $scope.sponsor = "";
+        //$scope.sponsor_name = "";
+        //$scope.sponsor = "";
+        $scope.showSponsorName = false;
+        
         $scope.update_sponsor = function(user){
             $scope.sponsor = user.id;
             $scope.sponsor_name = user.name; 
         };
         
         $scope.check_sponsor = function(){
-            if($scope.sponsor.length > 1){
-                return "open";
-            }else{
-                $scope.sponsor_name = "";
-                return false;
+            if($scope.hasOwnProperty("sponsor")){
+                if($scope.sponsor.length > 1){
+                    $scope.showSponsorName = true;
+                    return "open";
+                }
             }
+            $scope.showSponsorName = false;
+                //$scope.sponsor_name = "";
+            return false;
         };
         
         $scope.showName = function(){
-            if($scope.sponsor.length > 1 && $scope.sponsor_name !== ""){
-                return true;
-            }else return false; 
+            if($scope.hasOwnProperty("sponsor") && $scope.hasOwnProperty("sponsor_name")){
+                if($scope.sponsor.length > 1 && $scope.sponsor_name !== ""){
+                    $scope.showSponsorName = true;
+                    return true;
+                }
+            }
+            return false; 
         };
     }]);
 }(module, pushIfNotFound, checkExists, ControlPad));
