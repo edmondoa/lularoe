822bdc490e58a01a0d6d842c3f23736a069960ab
Matthew Frederico <mfrederico@gmail.com>
2015-04-14 19:49:34 -0600

    Receipt layout stuff

--- app/views/layouts/main-menu.blade.php
+++ app/views/layouts/main-menu.blade.php
@@ -1,8 +1,15 @@
- 			<div class="list-group" id="main-menu">
- 				@if ((Auth::check()))
+@if ((Auth::check()))
+ 			<div class="list-group hidden-print" id="main-menu">
  					<a title="Dashboard" href="/dashboard" class="list-group-item"><i class="fa fa-dashboard"></i> <span class="text">Dashboard</span></a>
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Rep'])))
+ 					<a href="javascript:void(0)" data-href="/reports" class='list-group-item' data-toggle="popover" data-content="
+ 						<a href='/reports/sales'><i class='fa fa-file'></i> Sales</a>
+ 						<a href='/reports/orders'><i class='fa fa-plus'></i> Orders</a>
+ 					">
+	 					<i class="fa fa-bar-chart"></i> <span class="text">Reports</span>
+ 					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Rep'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Rep'])))
  					<a href="javascript:void(0)" data-href="/posts" class='list-group-item' data-toggle="popover" data-content="
  						<a target='_blank' href='/public-posts'><i class='fa fa-globe'></i> Public Announcements</a>
  						<a href='/posts'><i class='fa fa-user'></i> Announcements for {{ Config::get('site.rep_title') }}</a>
@@ -13,7 +20,7 @@
 	 					<i class="fa fa-thumb-tack"></i> <span class="text">Announcements</span>
 	 				</a>
 	 			@endif
-	 			@if ((Auth::check())&&(Auth::user()->hasRole(['Rep'])))
+	 			@if ((Auth::user()->hasRole(['Rep'])))
  					<a href="javascript:void(0)" data-href="/events" class='list-group-item' data-toggle="popover" data-content="
  						<a target='_blank' href='/public-events'><i class='fa fa-globe'></i> Public Events Page</a>
  						<a href='/events'><i class='fa fa-calendar'></i> Upcoming Events for FC's</a>
@@ -31,7 +38,7 @@
  						<i class="fa fa-users"></i> <span class="text">Contacts</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin'])))
  					<a href="javascript:void(0)" data-href="/events" class='list-group-item' data-toggle="popover" data-content="
  						<a target='_blank' href='/public-events'><i class='fa fa-globe'></i> Public Events Page</a>
  						<a href='/events'><i class='fa fa-calendar'></i> Upcoming Events</a>
@@ -41,7 +48,7 @@
  						<i class="fa fa-calendar"></i> <span class="text">Company Events</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
  				    <a href="javascript:void(0)" data-href="/inventories" class='list-group-item' data-toggle="popover" data-content="
 						 <a href='/products'><i class='fa fa-female'></i> My Inventory</a>
  						 <!-- <a href='/products/create'><i class='fa fa-plus'></i> Add Inventory</a> -->
@@ -50,7 +57,7 @@
                          <i class="fa fa-female"></i> <span class="text">Inventory</span>
                      </a>
                 @endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
  					<a href="javascript:void(0)" data-href="/pages" class='list-group-item' data-toggle="popover" data-content="
  						<a href='/pages'><i class='fa fa-file'></i> All Pages</a>
  						<a href='/pages/create'><i class='fa fa-plus'></i> New Page</a>
@@ -58,7 +65,7 @@
 						<i class="fa fa-file"></i> <span class="text">Pages</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Rep'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Rep'])))
  					<a href="javascript:void(0)" data-href="/parties" class='list-group-item' data-toggle="popover" data-content="
  						<a target='_blank' href='/public-parties'><i class='fa fa-globe'></i> Public Pop-Up Boutiques Page</a>
  						<a href='/parties'><i class='fa fa-users'></i> Upcoming Pop-Up Boutiques</a>
@@ -68,10 +75,10 @@
  						<i class="fa fa-users"></i> <span class="text">Pop-Up Boutiques</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
  					<a href="javascript:void(0)" data-href="/opportunities" class='list-group-item' data-toggle="popover" data-content="
  						<a href='/opportunities'><i class='fa fa-check'></i> All Promotions</a>
- 						@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
+ 						@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
  							<a href='/opportunities/create'><i class='fa fa-plus'></i> New Promotion</a>
  						@endif
  					">
@@ -85,10 +92,10 @@
                          <i class="fa fa-bank"></i> <span class="text">Sales</span>
                      </a>
                 @endif
-                @if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin'])))
+                @if ((Auth::user()->hasRole(['Superadmin','Admin'])))
                  	<!-- <a title="Site Settings" href="/config" class="list-group-item"><span class="fa fa-cog"></span> <span class="text">Settings</span></a> -->
                 @endif
-                @if ((Auth::check())&&(Auth::user()->hasRole(['Rep'])))
+                @if ((Auth::user()->hasRole(['Rep'])))
  					<a href="javascript:void(0)" data-href="/user-sites" class='list-group-item' data-toggle="popover" data-content="
  						<a target='_blank' href='//{{ Auth::user()->public_id }}.{{ Config::get("site.base_domain") }}'><i class='fa fa-eye'></i> View Site</a>
  						<a href='/user-sites/{{ Auth::user()->id }}/edit'><i class='fa fa-pencil'></i> Edit Site</a>
@@ -97,9 +104,9 @@
  						<span class="text">Site</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor','Rep'])))
  					<a href="javascript:void(0)" data-href="/downline" class='list-group-item' data-toggle="popover" data-content="
-		 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin'])))
+		 				@if ((Auth::user()->hasRole(['Superadmin','Admin'])))
 	 						<a href='/downline/new/0'><i class='fa fa-certificate'></i> New Team Members</a>
 	 						<a href='/downline/immediate/0'><i class='fa fa-arrow-down'></i> Sponsored Members</a>
 	 						<a href='/downline/all/0'><i class='fa fa-bars'></i> Entire Team</a>
@@ -115,12 +122,12 @@
  						<span class="text">Team</span>
  					</a>  
   					<a href="javascript:void(0)" data-href="/media" class='list-group-item' data-toggle="popover" data-content="
- 						@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
+ 						@if ((Auth::user()->hasRole(['Superadmin','Admin','Editor'])))
  							<a href='/media/user/{{ Config::get('site.admin_uid') }}'><i class='fa fa-th-large'></i> Company Tools/Assets</a>
  							<a href='/media-shared-with-reps'><i class='fa fa-th-large'></i> Tools/Assets Shared with {{ Config::get('site.rep_title') }}</a>
  							<!-- <a href='/media-reps'><i class='fa fa-users'></i> Tools/Assets Uploaded by {{ Config::get('site.rep_title') }}</a> -->
  						@endif
- 						@if ((Auth::check())&&(Auth::user()->hasRole(['Rep'])))
+ 						@if ((Auth::user()->hasRole(['Rep'])))
  							<a href='/media-shared-with-reps'><i class='fa fa-th-large'></i> All Tools/Assets</a>
  						@endif
   							<a href='/media-shared-with-reps?filter=Branding'><i class='fa fa-th-large'></i> Branding</a>
@@ -136,7 +143,7 @@
  						<i class="fa fa-file-image-o"></i> <span class="text">Tools/Assets</span>
  					</a>
  				@endif
- 				@if ((Auth::check())&&(Auth::user()->hasRole(['Superadmin','Admin'])))
+ 				@if ((Auth::user()->hasRole(['Superadmin','Admin'])))
  					<a href="javascript:void(0)" data-href="/users" class='list-group-item' data-toggle="popover" data-content="
  						<a href='/users'><i class='fa fa-user'></i> All Users</a>
  						<a href='/users/create'><i class='fa fa-plus'></i> New User</a>
@@ -148,3 +155,4 @@
 					<!-- <a href="/smsMessages" class="list-group-item"><i class="fa fa-mobile-phone"></i> <span class="text">SmsMessages</span></a> -->
 				@endif
  			</div>
+	@endif
