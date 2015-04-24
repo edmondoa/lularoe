6aa351dc4629a06884fed90144d065ce2e9ae0c7
Matthew Frederico <mfrederico@gmail.com>
2015-04-24 00:12:57 -0600

    for mwl beta

--- app/config/mattzilla/site.php
+++ app/config/mattzilla/site.php
@@ -1,6 +1,8 @@
 <?php
     return [
             'domain' => 'mylularoe.local',
+			'mwl_api'       => 'http://localhost:8080/cms/',
+
 			'warehouse_email' => 'm.fred@cyberteknix.com',
             'contact_email' => 'mfrederico@gmail.com',
             'base_domain' => 'mylularoe.local',
