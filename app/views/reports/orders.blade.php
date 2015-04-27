d4930daf7e0acfeb3123abbf92f907166fa6f96f
Matthew Frederico (cyberteknix) <m.fred@cyberteknix.com>
2015-04-27 14:53:12 -0600

    shipped and non shipped

--- app/views/reports/orders.blade.php
+++ app/views/reports/orders.blade.php
@@ -7,12 +7,40 @@
 		<div role="tabpanel">
 			<ul class="nav nav-tabs" role="tablist">
 				<li role="presentation" class="nav active"><a href="#received" role="tab" data-toggle="tab">Received</a></li>
-				<!-- <li role="presentation" class="nav"><a href="#shipped" role="tab" data-toggle="tab">Shipped</a></li> -->
+				<li role="presentation" class="nav"><a href="#shipped" role="tab" data-toggle="tab">Shipped</a></li>
 			</ul>
 
 			<div class="tab-content">
 				<div role="tabpanel" class="well tab-pane fade col col-md-12" id="shipped">
 					<h3>Orders - Shipped</h3>
+					<table class="table table-striped table-hover">
+						<thead>
+							<tr>
+								<th class="header">Date Shipped</th><th>Num</th><th>Person</th><th>Amount</th><th></th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($orderlist as $receipt) 
+							@if ($receipt->shipped != '0000-00-00 00:00:00')
+							<tr class="media">
+								<td><div>{{$receipt->shipped}}</div></td>
+								<td><div>{{$receipt->id}}</div></td>
+								<td><div>{{$receipt->to_firstname}} {{$receipt->to_lastname}}</div></td>
+								<td>
+									@if ($receipt->tax > 0) <div>{{$receipt->tax}}</div> @endif
+									<div>{{$receipt->subtotal}}</div>
+								</td>
+								<td>
+									<a href="/invoice/view/{{$receipt->id}}" TARGET="_BLANK" class="btn btn-smi btn-default">View</a>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="100">{{$receipt->note}}</td>
+							</tr>
+							@endif 
+							@endforeach
+						</tbody>
+					</table>
 				</div>
 
 				 <div role="tabpanel" class="well tab-pane fade in active col col-md-12" id="received">
@@ -20,11 +48,12 @@
 					<table class="table table-striped table-hover">
 						<thead>
 							<tr>
-								<th class="header">Date</th><th>Num</th><th>Person</th><th>Amount</th><th>Items</th>
+								<th class="header">Date</th><th>Num</th><th>Person</th><th>Amount</th><th></th>
 							</tr>
 						</thead>
 						<tbody>
 							@foreach ($orderlist as $receipt) 
+							@if ($receipt->shipped == '0000-00-00 00:00:00')
 							<tr class="media">
 								<td><div>{{$receipt->created_at}}</div></td>
 								<td><div>{{$receipt->id}}</div></td>
@@ -34,12 +63,13 @@
 									<div>{{$receipt->subtotal}}</div>
 								</td>
 								<td>
-										<a href="/invoice/view/{{$receipt->id}}" TARGET="_BLANK" class="btn btn-smi btn-default">Show Invoice</a>
+									<a href="/invoice/view/{{$receipt->id}}" TARGET="_BLANK" class="btn btn-smi btn-default">View</a>
 								</td>
 							</tr>
 							<tr>
 								<td colspan="100">{{$receipt->note}}</td>
 							</tr>
+							@endif 
 							@endforeach
 						</tbody>
 					</table>
