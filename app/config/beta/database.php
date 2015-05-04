73a16fd8ddd44b2e2788051fd51dfbf2cd15314d
Jake Barlow <jake@jakebarlow.com>
2015-05-04 17:11:31 -0600

    beta environment settings

--- app/config/beta/database.php
+++ app/config/beta/database.php
@@ -6,9 +6,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'mwl-beta.controlpad.com',
-			'database'  => 'llr',
-			'username'  => 'llr',
-			'password'  => '',
+			'database'  => 'llr_web',
+			'username'  => 'llr_web',
+			'password'  => '7U8$SAV*NEjuB$T%',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
